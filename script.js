e71625b3f60c2060a84b9b16b3b580738c6c9cda
franciscohgoulart-2213 <francisco_h_machado@estudante.sesisenai.org.br>
2026-03-17 08:47:15 -0300

    add-exercicio 1

---
+++ script.js
@@ -0,0 +1,23 @@
+function somarNumeros () {
+let soma = 0;
+let continuar = true;
+
+while (continuar) {
+    let numero = parseFloat(prompt("Digite um numero ( ou digite ('0') para encerrar):"));
+
+if (!isNaN(numero)) {
+
+    soma += numero;
+
+} else {
+    alert("Por favor, digite um numero valido.");
+}
+
+continuar = confirm ("Deseja adicionar mais numeros?")
+
+}
+    alert("A soma dos números é: " + soma);
+}
+
+
+somarNumeros();
