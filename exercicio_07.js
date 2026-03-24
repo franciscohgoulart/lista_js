70dcfa3f3b94e7277c778b3d6f7c561f329fcbc7
franciscohgoulart-2213 <francisco_h_machado@estudante.sesisenai.org.br>
2026-03-24 09:13:02 -0300

    add-atividade 7

---
+++ exercicio_07.js
@@ -0,0 +1,64 @@
+function picoleteria () {
+
+let codigo = prompt ("Dgite o codigo de um sorvete, 'a' ate 'i'");
+
+if (codigo =="a") {
+    alert ("chocolate R$1,50");
+}
+else if (codigo =="b") {
+    alert ("chocolate R$2,50");
+}
+else if (codigo =="c") {
+    alert ("chocolate R$2,50");
+}
+else if (codigo =="d") {
+    alert ("chocolate R$3,20");
+}
+else if (codigo =="e") {
+    alert ("chocolate R$3,40");
+}
+else if (codigo =="f") {
+    alert ("Vanilla Ice R$3,00");
+}
+else if (codigo =="g") {
+    alert ("Céu Azul R$3,60");
+}
+else if (codigo =="h") {
+    alert ("Brownie R$4,00");
+}
+else if (codigo =="i") {
+    alert ("Hawaino R$5,00");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
